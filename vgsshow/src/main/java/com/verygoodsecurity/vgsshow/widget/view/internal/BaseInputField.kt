464ce096cb5c120c108615ca7204bd12c7119b7d
Dmytro <dmitriy.kosolapenko@vgs.io>
2020-10-27 16:20:49 +0200

    deny textWatchers

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/internal/BaseInputField.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/internal/BaseInputField.kt
@@ -1,10 +1,16 @@
 package com.verygoodsecurity.vgsshow.widget.view.internal
 
 import android.content.Context
+import android.text.TextWatcher
 import androidx.appcompat.widget.AppCompatTextView
 
 class BaseInputField(context: Context) : AppCompatTextView(context) {
 
     var fieldName: String? = null
 
+    override fun setFocusable(focusable: Boolean) {
+        super.setFocusable(false)
+    }
+
+    override fun addTextChangedListener(watcher: TextWatcher?) {}
 }
