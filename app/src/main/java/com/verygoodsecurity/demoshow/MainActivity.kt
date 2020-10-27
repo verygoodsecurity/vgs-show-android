4735488a4ebd1eeffc82f2b927a1e6a5de118cc5
Dmytro <dmitriy.kosolapenko@vgs.io>
2020-10-27 13:43:10 +0200

    add UI

--- app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
@@ -2,10 +2,28 @@ package com.verygoodsecurity.demoshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.verygoodsecurity.vgsshow.core.VGSShow
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val show = VGSShow()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        show.bindView(vgsSecureView)
+
+        btn?.setOnClickListener {
+            revealData()
+        }
     }
+
+    private fun revealData() {
+        show.request("fieldName", "123")
+        show.request("fieldName_2", "123")
+        show.request("someName", "123")
+    }
+
 }
