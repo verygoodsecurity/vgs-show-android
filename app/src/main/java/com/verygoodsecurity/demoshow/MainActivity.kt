0db9dcba1053c86b7288359146a22b6abe9f5981
Dmytro <dmitriy.kosolapenko@vgs.io>
2020-10-27 17:16:34 +0200

    update UI for testing

--- app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
@@ -2,6 +2,9 @@ package com.verygoodsecurity.demoshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.verygoodsecurity.vgsshow.core.VGSShow
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,17 +16,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        show.bindView(vgsSecureView)
+        setupTextView()
 
         btn?.setOnClickListener {
             revealData()
         }
+        Testbtn?.setOnClickListener {
+            testfunc()
+        }
+    }
+
+    private fun setupTextView() {
+        show.bindView(vgsSecureView)
+
+        vgsSecureView.setOnClickListener {
+            Log.e("test", "CLICK $it")
+        }
     }
 
     private fun revealData() {
+        parentV.requestLayout()
+        vgsSecureView.requestLayout()
+        vgsSecureView.invalidate()
+
         show.request("fieldName", "123")
         show.request("fieldName_2", "123")
         show.request("someName", "123")
     }
 
+    fun testfunc() {
+        vgsSecureView?.requestFocus()
+        for(ch in 0..vgsSecureView.childCount) {
+            val child = vgsSecureView.getChildAt(ch)
+
+            if(child is TextView) {
+                Log.e("test", "getText() = ${child.text}")
+                Log.e("test", "getEditableText() = ${child.editableText}")
+                child.addTextChangedListener {
+                    Log.e("test", "TEXT_WATHCER $it")
+                }
+            }
+        }
+    }
 }
