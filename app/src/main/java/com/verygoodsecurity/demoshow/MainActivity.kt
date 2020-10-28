95d32bf907cf6848143b8844b593e5e09a5d408c
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-10-28 18:02:01 +0200

    Add test code to MainActivity.kt

--- app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/demoshow/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.verygoodsecurity.demoshow
 
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
 import com.verygoodsecurity.vgsshow.VGSShow
 import com.verygoodsecurity.vgsshow.core.Environment
+import com.verygoodsecurity.vgsshow.core.listener.VGSResponseListener
+import com.verygoodsecurity.vgsshow.core.network.model.VGSResponse
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), VGSResponseListener {
 
     private val showVgs: VGSShow by lazy {
         VGSShow(this, "tntaq8uft80", Environment.SANDBOX)
@@ -16,10 +20,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test?.setOnClickListener {
+        sendRequest?.setOnClickListener {
             thread(start = true) {
-                showVgs.request("card_number_3", "tok_sandbox_nj9DWPJMFaP8U3HqXQ2DE")
+                makeTestRequest()
             }
         }
+        addListener?.setOnClickListener {
+                showVgs.addResponseListener(object : VGSResponseListener {
+
+                    override fun onResponse(response: VGSResponse) {
+
+                    }
+                })
+        }
+        addSameListener?.setOnClickListener {
+                showVgs.addResponseListener(this)
+        }
+        removeListener?.setOnClickListener {
+                showVgs.removeResponseListener(this)
+        }
+        clearListener?.setOnClickListener {
+                showVgs.clearResponseListeners()
+        }
+    }
+
+    override fun onResponse(response: VGSResponse) {
+
+    }
+
+    @WorkerThread
+    private fun makeTestRequest() {
+        val response = showVgs.request("card_number_3", "tok_sandbox_nj9DWPJMFaP8U3HqXQ2DE")
+        Log.d(VGSShow::class.simpleName, response.toString())
     }
 }
