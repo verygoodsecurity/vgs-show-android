e3ef61c47ff11f9db592b0ef1045122795a5b0da
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-10-29 12:18:22 +0200

    Fix all visibility modifiers

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/network/client/model/HttpResponse.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/network/client/model/HttpResponse.kt
@@ -1,6 +1,6 @@
 package com.verygoodsecurity.vgsshow.core.network.client.model
 
-data class HttpResponse(
+internal data class HttpResponse(
     val code: Int,
     val isSuccessful: Boolean,
     val message: String? = null,
