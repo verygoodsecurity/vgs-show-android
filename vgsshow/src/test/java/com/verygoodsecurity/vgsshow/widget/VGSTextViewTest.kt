ea72daee73be2b3bcef13d263cd5072fdc7198db
Dmytro Kos <dmitriy.kosolapenko@vgs.io>
2020-10-29 16:27:20 +0300

    add style attibutes

--- vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/VGSTextViewTest.kt
+++ vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/VGSTextViewTest.kt
@@ -1,6 +1,9 @@
 package com.verygoodsecurity.vgsshow.widget
 
 import android.app.Activity
+import android.graphics.Color
+import android.graphics.Typeface
+import android.view.Gravity
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -52,5 +55,111 @@ class VGSTextViewTest {
 
         val state = view.getState()
         assertEquals(state?.fieldName, fn)
+    }
+
+    @Test
+    fun test_paddings() {
+        view.onAttachedToWindow()
+
+        view.setPadding(30,20,10,0)
+
+        assertEquals(view.paddingLeft, 30)
+        assertEquals(view.paddingTop, 20)
+        assertEquals(view.paddingRight, 10)
+        assertEquals(view.paddingBottom, 0)
+
+        val state = view.getState()
+        assertNotNull(state)
+        assertEquals(state!!.paddingLeft, 30)
+        assertEquals(state.paddingTop, 20)
+        assertEquals(state.paddingRight, 10)
+        assertEquals(state.paddingBottom, 0)
+    }
+
+    @Test
+    fun test_gravity() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.setGravity(Gravity.NO_GRAVITY)
+        assertEquals(state!!.gravity, Gravity.NO_GRAVITY)
+
+        view.setGravity(Gravity.CENTER)
+        assertEquals(state.gravity, Gravity.CENTER)
+
+        view.setGravity(Gravity.TOP or Gravity.END)
+        assertEquals(state.gravity, Gravity.TOP or Gravity.END)
+    }
+
+    @Test
+    fun test_singleline() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.setSingleLine(true)
+        assertEquals(state!!.isSingleLine, true)
+
+        view.setSingleLine(false)
+        assertEquals(state.isSingleLine, false)
+    }
+
+    @Test
+    fun test_text_size() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.setTextSize(21f)
+        assertEquals(state!!.textSize, 21f)
+
+        view.setTextSize(25f)
+        assertEquals(state.textSize, 25f)
+    }
+
+    @Test
+    fun test_text_color() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.setTextColor(Color.BLUE)
+        assertEquals(state!!.textColor, Color.BLUE)
+
+        view.setTextColor(Color.YELLOW)
+        assertEquals(state.textColor, Color.YELLOW)
+    }
+
+    @Test
+    fun test_enabled() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.isEnabled = true
+        assertEquals(state!!.enabled, true)
+
+        view.isEnabled = false
+        assertEquals(state.enabled, false)
+    }
+
+    @Test
+    fun test_enabsled() {
+        view.onAttachedToWindow()
+
+        val state = view.getState()
+        assertNotNull(state)
+
+        view.setTypeface(Typeface.SERIF)
+        assertEquals(state!!.typeface, Typeface.SERIF)
+
+        view.setTypeface(Typeface.DEFAULT)
+        assertEquals(state.typeface, Typeface.DEFAULT)
     }
 }
