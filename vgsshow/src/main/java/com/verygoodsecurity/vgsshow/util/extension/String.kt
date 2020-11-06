dcda73b6586cf06c3c2f9f5bde811c9e73b5419c
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-11-06 17:20:17 +0200

    Code style fix

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/String.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/String.kt
@@ -18,7 +18,9 @@ internal infix fun String.concatWithDash(suffix: String): String = when {
     else -> this + DASH + suffix
 }
 
+private const val MD_5_ALGORITHM = "MD5"
+
 internal fun String.toMD5(): String {
-    val bytes = MessageDigest.getInstance("MD5").digest(this.toByteArray())
+    val bytes = MessageDigest.getInstance(MD_5_ALGORITHM).digest(this.toByteArray())
     return bytes.joinToString("") { "%02x".format(it) }
 }
