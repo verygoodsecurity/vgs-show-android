bf21ad9ce0ae37287da99ca4c86c7ecf9eb54f05
Dmytro <dmitriy.kosolapenko@vgs.io>
2020-11-09 16:51:29 +0200

    fix issue

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/Regex.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/Regex.kt
@@ -1,13 +1,9 @@
 package com.verygoodsecurity.vgsshow.util.extension
 
-import java.util.regex.PatternSyntaxException
-
 fun String.transformWithRegex(text: String, replacement: String): String {
     return try {
         this.toRegex().replace(text, replacement)
-    } catch (ex: PatternSyntaxException) {
-        text
-    } catch (ex: IndexOutOfBoundsException) {
+    } catch (ex: Exception) {
         text
     }
 }
