da54acf1e3528578be5948c4c7f8df1035bcc6fa
DmytroDm <72735363+DmytroDm@users.noreply.github.com>
2020-11-12 17:54:04 +0200

    Add letter spacing attr, add tests, fix network tests (#31)
    * Add letter spacing attr, add tests, fix network tests
    
    * Improve tests

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/FieldStateImpl.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/FieldStateImpl.kt
@@ -1,12 +1,15 @@
 package com.verygoodsecurity.vgsshow.widget.view
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Typeface
+import android.os.Build
 import android.os.Parcelable
 import android.text.InputType
 import android.util.TypedValue
 import android.view.Gravity
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.annotation.VisibleForTesting
 import com.verygoodsecurity.vgsshow.widget.VGSTextView
 import com.verygoodsecurity.vgsshow.widget.view.internal.BaseInputField
@@ -165,6 +168,14 @@ internal class FieldStateImpl(
         set(value) {
             field = value
             this@FieldStateImpl.field.typeface = value
+        }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    internal var letterSpacing: Float = 0f
+        @SuppressLint("NewApi")
+        set(value) {
+            field = value
+            this@FieldStateImpl.field.letterSpacing = value
         }
 
     fun attachTo(inputFieldView: VGSTextView) {
