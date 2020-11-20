f2bf080c471f86186841839661cfc7d56744fc9d
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-11-20 11:59:03 +0200

    Refactoring

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/String.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/util/extension/String.kt
@@ -1,7 +1,5 @@
 package com.verygoodsecurity.vgsshow.util.extension
 
-import java.security.MessageDigest
-
 private const val SLASH = "/"
 
 internal infix fun String.concatWithSlash(suffix: String): String = when {
@@ -16,11 +14,4 @@ internal infix fun String.concatWithDash(suffix: String): String = when {
     suffix.isEmpty() -> this
     suffix.startsWith(DASH) -> this + suffix
     else -> this + DASH + suffix
-}
-
-private const val MD_5_ALGORITHM = "MD5"
-
-internal fun String.toMD5(): String {
-    val bytes = MessageDigest.getInstance(MD_5_ALGORITHM).digest(this.toByteArray())
-    return bytes.joinToString("") { "%02x".format(it) }
 }
