bbdd80ab78a798cd8a504b080101ae44d4be1482
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-12-29 13:58:18 +0200

    Update tests

--- vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/text/method/SecureTransformationMethodTest.kt
+++ vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/text/method/SecureTransformationMethodTest.kt
@@ -1,14 +1,20 @@
 package com.verygoodsecurity.vgsshow.widget.text.method
 
 import com.verygoodsecurity.vgsshow.widget.view.textview.method.SecureTransformationMethod
+import com.verygoodsecurity.vgsshow.widget.view.textview.method.VGSSecureTextOptions
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class RangePasswordTransformationMethodTest {
+class SecureTransformationMethodTest {
+
+    private val fullRange = IntRange(Int.MIN_VALUE, Int.MAX_VALUE)
+    private val options = VGSSecureTextOptions.Builder().build()
+    private val ignoreTransformationOptions =
+        VGSSecureTextOptions.Builder().isIgnoreTransformationRegex(true).build()
 
     @Test
     fun test_1() {
-        val charSequence = SecureTransformationMethod()
+        val charSequence = SecureTransformationMethod(fullRange, ignoreTransformationOptions)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -16,7 +22,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_2() {
-        val charSequence = SecureTransformationMethod(6, 12)
+        val range = IntRange(6, 12)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("424242••••••4242", charSequence.toString())
@@ -24,7 +31,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_3() {
-        val charSequence = SecureTransformationMethod(start = -12)
+        val range = IntRange(-12, 100)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -32,7 +40,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_4() {
-        val charSequence = SecureTransformationMethod(start = 77)
+        val range = IntRange(101, 100)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -40,7 +49,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_5() {
-        val charSequence = SecureTransformationMethod(start = 3)
+        val range = IntRange(3, 100)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("424•••••••••••••", charSequence.toString())
@@ -48,7 +58,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_6() {
-        val charSequence = SecureTransformationMethod(7, 3)
+        val range = IntRange(7, 3)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -56,7 +67,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_7() {
-        val charSequence = SecureTransformationMethod(end = 3)
+        val range = IntRange(0, 3)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("•••2424242424242", charSequence.toString())
@@ -64,7 +76,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_8() {
-        val charSequence = SecureTransformationMethod(end = -3)
+        val range = IntRange(0, -3)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -72,7 +85,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_9() {
-        val charSequence = SecureTransformationMethod(end = 88)
+        val range = IntRange(0, 88)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -80,7 +94,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_10() {
-        val charSequence = SecureTransformationMethod(3, 3)
+        val range = IntRange(3, 3)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation(EXAMPLE, null)
 
         assertEquals("4242424242424242", charSequence.toString())
@@ -88,7 +103,8 @@ class RangePasswordTransformationMethodTest {
 
     @Test
     fun test_11() {
-        val charSequence = SecureTransformationMethod(3, 3)
+        val range = IntRange(3, 3)
+        val charSequence = SecureTransformationMethod(range, options)
             .getTransformation("", null)
 
         assertEquals("", charSequence.toString())
@@ -97,13 +113,23 @@ class RangePasswordTransformationMethodTest {
     @Test
     fun test_12() {
         val text = "*&&^%\$#@!()_+_\\\\}{}\$"
-        val charSequence = SecureTransformationMethod()
+
+        val charSequence = SecureTransformationMethod(fullRange, ignoreTransformationOptions)
             .getTransformation(text, null)
 
         assertEquals("••••••••••••••••••••", charSequence.toString())
     }
 
+    @Test
+    fun test_13() {
+        val charSequence = SecureTransformationMethod(fullRange, options)
+            .getTransformation(EXAMPLE_WITH_TRANSFORMATION, null)
+
+        assertEquals("••••-••••-••••-••••", charSequence.toString())
+    }
+
     companion object {
         private const val EXAMPLE = "4242424242424242"
+        private const val EXAMPLE_WITH_TRANSFORMATION = "4242-4242-4242-4242"
     }
 }
