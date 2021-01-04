4849bbaa6da7538c569c3560eff9c28ad3e2ae41
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-04 13:41:15 +0200

    Add VGSSecureTextOptions.kt comments

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/textview/method/VGSSecureTextOptions.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/textview/method/VGSSecureTextOptions.kt
@@ -3,6 +3,9 @@ package com.verygoodsecurity.vgsshow.widget.view.textview.method
 private const val DEFAULT_SECURE_REPLACEMENT = '•'
 private const val DEFAULT_IGNORE_TRANSFORMATION_REGEX = true
 
+/**
+ * Secure text options definition class.
+ */
 class VGSSecureTextOptions private constructor(
     val secureSymbol: Char,
     val ignoreTransformationRegex: Boolean
@@ -14,11 +17,26 @@ class VGSSecureTextOptions private constructor(
 
         private var ignoreTransformationRegex: Boolean = DEFAULT_IGNORE_TRANSFORMATION_REGEX
 
+        /**
+         *  Apply custom secure symbol.
+         *
+         *  @param secureSymbol custom secure char.
+         */
         fun setSecureSymbol(secureSymbol: Char) = apply { this.secureSymbol = secureSymbol }
 
+        /**
+         * Decline should transformation regex be ignored by secure transformation or not
+         *
+         * @param isIgnore true if should be ignore and all symbols should be secured, false otherwise.
+         */
         fun setIsIgnoreTransformationRegex(isIgnore: Boolean) =
             apply { this.ignoreTransformationRegex = isIgnore }
 
+        /**
+         * Build VGSSecureTextOptions object.
+         *
+         * @return configured VGSSecureTextOptions.
+         */
         fun build() = VGSSecureTextOptions(secureSymbol, ignoreTransformationRegex)
     }
 }
