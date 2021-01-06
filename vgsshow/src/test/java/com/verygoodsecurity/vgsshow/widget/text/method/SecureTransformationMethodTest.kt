2c47d91a0b48e6cf7846d33f888afa56c3e90ee7
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-06 11:49:08 +0200

    Update tests

--- vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/text/method/SecureTransformationMethodTest.kt
+++ vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/text/method/SecureTransformationMethodTest.kt
@@ -1,19 +1,18 @@
 package com.verygoodsecurity.vgsshow.widget.text.method
 
 import com.verygoodsecurity.vgsshow.widget.view.textview.method.SecureTransformationMethod
-import com.verygoodsecurity.vgsshow.widget.view.textview.method.VGSSecureTextOptions
-import com.verygoodsecurity.vgsshow.widget.view.textview.method.VGSSecureTextRange
+import com.verygoodsecurity.vgsshow.widget.view.textview.model.VGSTextRange
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class SecureTransformationMethodTest {
 
-    private val fullRange = VGSSecureTextRange(Int.MIN_VALUE, Int.MAX_VALUE)
-    private val options = VGSSecureTextOptions.Builder().build()
+    private val fullRange = VGSTextRange()
+    private val secureSymbol =  '•'
 
     @Test
     fun test_1() {
-        val charSequence = SecureTransformationMethod(arrayOf(fullRange), options)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(fullRange))
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -21,17 +20,17 @@ class SecureTransformationMethodTest {
 
     @Test
     fun test_2() {
-        val range = VGSSecureTextRange(6, 12)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(end = 2)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("424242••••••4242", charSequence.toString())
+        assertEquals("•••2424242424242", charSequence.toString())
     }
 
     @Test
     fun test_3() {
-        val range = VGSSecureTextRange(-12, 100)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(0)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -39,44 +38,45 @@ class SecureTransformationMethodTest {
 
     @Test
     fun test_4() {
-        val range = VGSSecureTextRange(101, 100)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(-1, 5)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("••••••••••••••••", charSequence.toString())
+        assertEquals(EXAMPLE, charSequence.toString())
     }
 
     @Test
     fun test_5() {
-        val range = VGSSecureTextRange(3, 100)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(0, -1)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("424•••••••••••••", charSequence.toString())
+        assertEquals(EXAMPLE, charSequence.toString())
     }
+
 
     @Test
     fun test_6() {
-        val range = VGSSecureTextRange(7, 3)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(5,4)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("••••••••••••••••", charSequence.toString())
+        assertEquals(EXAMPLE, charSequence.toString())
     }
 
     @Test
     fun test_7() {
-        val range = VGSSecureTextRange(0, 3)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(100, 5)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("•••2424242424242", charSequence.toString())
+        assertEquals(EXAMPLE, charSequence.toString())
     }
 
     @Test
     fun test_8() {
-        val range = VGSSecureTextRange(0, -3)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(0, 100)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
         assertEquals("••••••••••••••••", charSequence.toString())
@@ -84,49 +84,51 @@ class SecureTransformationMethodTest {
 
     @Test
     fun test_9() {
-        val range = VGSSecureTextRange(0, 88)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+        val range = VGSTextRange(3, 3)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("••••••••••••••••", charSequence.toString())
+        assertEquals("424•424242424242", charSequence.toString())
     }
 
     @Test
     fun test_10() {
-        val range = VGSSecureTextRange(3, 3)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
-            .getTransformation(EXAMPLE, null)
-
-        assertEquals("4242424242424242", charSequence.toString())
-    }
-
-    @Test
-    fun test_11() {
-        val range = VGSSecureTextRange(3, 3)
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
-            .getTransformation("", null)
-
-        assertEquals("", charSequence.toString())
-    }
-
-    @Test
-    fun test_12() {
-        val range = VGSSecureTextRange(0, 3)
-        val rangeTwo = VGSSecureTextRange(5, 7)
-        val charSequence = SecureTransformationMethod(arrayOf(range, rangeTwo), options)
+        val range = VGSTextRange(0, 2)
+        val rangeTwo = VGSTextRange(5, 6)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range, rangeTwo))
             .getTransformation(EXAMPLE, null)
 
         assertEquals("•••24••242424242", charSequence.toString())
     }
 
     @Test
-    fun test_13() {
-        val range = VGSSecureTextRange(0, 3)
-        val options = VGSSecureTextOptions.Builder().setSecureSymbol('%').build()
-        val charSequence = SecureTransformationMethod(arrayOf(range), options)
+    fun test_11() {
+        val range = VGSTextRange(0, 2)
+        val rangeTwo = VGSTextRange(5, -6)
+        val charSequence = SecureTransformationMethod(secureSymbol, arrayOf(range, rangeTwo))
             .getTransformation(EXAMPLE, null)
 
-        assertEquals("%%%2424242424242", charSequence.toString())
+        assertEquals("•••2424242424242", charSequence.toString())
+    }
+
+    @Test
+    fun test_12() {
+        val range = VGSTextRange(0, 2)
+        val rangeTwo = VGSTextRange(5, -6)
+        val charSequence = SecureTransformationMethod('#', arrayOf(range, rangeTwo))
+            .getTransformation(EXAMPLE, null)
+
+        assertEquals("###2424242424242", charSequence.toString())
+    }
+
+    @Test
+    fun test_13() {
+        val range = VGSTextRange(0, 2)
+        val rangeTwo = VGSTextRange(5, -6)
+        val charSequence = SecureTransformationMethod('#', arrayOf(range, rangeTwo))
+            .getTransformation("", null)
+
+        assertEquals("", charSequence.toString())
     }
 
     companion object {
---
+++ vgsshow/src/test/java/com/verygoodsecurity/vgsshow/widget/view/textview/model/VGSTextRangeTest.kt
@@ -0,0 +1,79 @@
+package com.verygoodsecurity.vgsshow.widget.view.textview.model
+
+import junit.framework.Assert.*
+import org.junit.Test
+
+class VGSTextRangeTest {
+
+    @Test
+    fun isValid_noParams_trueExpected() {
+        val range = VGSTextRange()
+        assertTrue(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_correctEnd_trueExpected() {
+        val range = VGSTextRange(end = 4)
+        assertTrue(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_zeroStart_trueExpected() {
+        val range = VGSTextRange(start = 0)
+        assertTrue(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_minusStart_falseExpected() {
+        val range = VGSTextRange(start = -1)
+        assertFalse(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_minusEnd_falseExpected() {
+        val range = VGSTextRange(end = -1)
+        assertFalse(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_startGreaterThanEnd_falseExpected() {
+        val range = VGSTextRange(5, 4)
+        assertFalse(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_endGreaterThanEnd_trueExpected() {
+        val range = VGSTextRange(0, 100)
+        assertTrue(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_equalStartEnd_trueExpected() {
+        val range = VGSTextRange(0, 0)
+        assertTrue(range.isValid(4))
+    }
+
+    @Test
+    fun isValid_endGreaterThanLength_endAsLengthExpected() {
+        val range = VGSTextRange(0, 100)
+        assertEquals(range.getEnd(4), 4)
+    }
+
+    @Test
+    fun isValid_correctParams_endAsLengthExpected() {
+        val range = VGSTextRange(0, 5)
+        assertEquals(range.getEnd(6), 6)
+    }
+
+    @Test
+    fun isValid_equalStartEndSmallerThanLength_endPlusOneExpected() {
+        val range = VGSTextRange(0, 0)
+        assertEquals(range.getEnd(6), 1)
+    }
+
+    @Test
+    fun isValid_startCorrect_startReturned() {
+        val range = VGSTextRange(0, 4)
+        assertEquals(range.getStart(), 0)
+    }
+}
