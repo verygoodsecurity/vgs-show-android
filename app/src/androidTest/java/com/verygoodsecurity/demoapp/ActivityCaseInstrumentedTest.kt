a62edd496731c8e7936b7aee4c22f30cc610ae84
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-11 11:27:27 +0200

    Implement secure text check

--- app/src/androidTest/java/com/verygoodsecurity/demoapp/ActivityCaseInstrumentedTest.kt
+++ app/src/androidTest/java/com/verygoodsecurity/demoapp/ActivityCaseInstrumentedTest.kt
@@ -11,6 +11,7 @@ import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
 import com.verygoodsecurity.demoapp.actions.SetTextAction
+import com.verygoodsecurity.demoapp.check.SecureTextCheck
 import com.verygoodsecurity.demoshow.MainActivity
 import com.verygoodsecurity.demoshow.R
 import org.hamcrest.Matchers.not
@@ -26,6 +27,7 @@ class ActivityCaseInstrumentedTest {
 
     companion object {
         const val CARD_NUMBER = "4111111111111111"
+        const val CARD_NUMBER_RESULT = "4111 - 1111 - 1111 - 1111"
         const val CARD_EXP_DATE = "02/2023"
     }
 
@@ -40,28 +42,38 @@ class ActivityCaseInstrumentedTest {
     }
 
     @Test
-    fun test_submit() {
+    fun test_reveal() {
         assertThat(device, notNullValue())
 
         val submitButton = interactWithSubmitButton()
+        val revealButton = interactWithRevealButton()
 
-        val cardInputField = interactWithCardNumber()
-        val cardExpDateInputField = interactWithCardExp()
+        val cardInputField = interactWithCardNumberInput()
+        val cardExpDateInputField = interactWithCardExpInput()
 
         val numberResponseToke = interactWithCardNumberResponse()
         val expirationResponseToke = interactWithExpDateResponse()
+
+        val revealedCardNumber = interactWithRevealCardNumber()
+        val revealedCardExpDate = interactWithRevealCardExpDate()
 
         cardInputField.perform(SetTextAction(CARD_NUMBER))
         cardExpDateInputField.perform(SetTextAction(CARD_EXP_DATE))
 
         performClick(submitButton)
-        pauseTestFor(5000)
+        pauseTestFor(3000)
 
         numberResponseToke.check(matches(not(withText(""))))
         expirationResponseToke.check(matches(not(withText(""))))
+
+        performClick(revealButton)
+        pauseTestFor(3000)
+
+        revealedCardNumber.check(SecureTextCheck(CARD_NUMBER_RESULT))
+//        revealedCardExpDate.check(SecureTextCheck(CARD_EXP_DATE))
     }
 
-    private fun interactWithCardNumber(): ViewInteraction {
+    private fun interactWithCardNumberInput(): ViewInteraction {
         val cardHolderField = onView(withId(R.id.cardNumber))
             .check(matches(not(isDisplayed())))
         onView(withId(R.id.cardNumberLayout))
@@ -70,7 +82,7 @@ class ActivityCaseInstrumentedTest {
         return cardHolderField
     }
 
-    private fun interactWithCardExp(): ViewInteraction {
+    private fun interactWithCardExpInput(): ViewInteraction {
         val cardInputField = onView(withId(R.id.expDate))
             .check(matches(not(isDisplayed())))
         onView(withId(R.id.expDateLayout))
@@ -87,6 +99,21 @@ class ActivityCaseInstrumentedTest {
 
     private fun interactWithSubmitButton(): ViewInteraction {
         return onView(withId(R.id.submitButton))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun interactWithRevealCardNumber(): ViewInteraction {
+        return onView(withId(R.id.number))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun interactWithRevealCardExpDate(): ViewInteraction {
+        return onView(withId(R.id.expiration))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun interactWithRevealButton(): ViewInteraction {
+        return onView(withId(R.id.requestButton))
             .check(matches(isDisplayed()))
     }
 
