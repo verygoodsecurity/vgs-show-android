a4c54bf92c2ea8049a92d88329dac286ad2f0003
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-01-11 11:58:30 +0200

    Update VGSTextRange.kt comments and visibility modifiers

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/textview/model/VGSTextRange.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/view/textview/model/VGSTextRange.kt
@@ -10,10 +10,14 @@ import kotlin.math.min
  *  @property end end of range (Inclusive).
  */
 data class VGSTextRange constructor(
-    @IntRange internal val start: Int = 0,
-    @IntRange internal val end: Int = Int.MAX_VALUE
+    @IntRange val start: Int = 0,
+    @IntRange val end: Int = Int.MAX_VALUE
 ) {
 
+    /**
+     * Check if start and end values is valid.
+     *
+     * @return true if range valid.
+     */
     fun isValid(textLength: Int): Boolean = end >= 0 && start in 0..min(end, textLength)
-
 }
