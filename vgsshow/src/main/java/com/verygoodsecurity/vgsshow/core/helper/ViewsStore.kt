e922f895cbb6f865e9ee93971c0e70f50058e605
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-01 12:52:49 +0200

    Add view upadte logs

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/helper/ViewsStore.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/helper/ViewsStore.kt
@@ -2,6 +2,8 @@ package com.verygoodsecurity.vgsshow.core.helper
 
 import androidx.annotation.MainThread
 import com.verygoodsecurity.vgsshow.core.network.model.data.response.ResponseData
+import com.verygoodsecurity.vgsshow.util.extension.logDebug
+import com.verygoodsecurity.vgsshow.util.extension.logWaring
 import com.verygoodsecurity.vgsshow.widget.VGSTextView
 import com.verygoodsecurity.vgsshow.widget.core.VGSView
 
@@ -19,18 +21,41 @@ internal class ViewsStore {
 
     @MainThread
     fun update(data: ResponseData?) {
+        logStartViewsUpdate()
+        val unrevealedContentPaths = mutableListOf<String>()
         for (view in views) {
             if (view.ignoreField) {
                 continue
             }
-            when (view) {
-                is VGSTextView -> data?.getValue(view.getContentPath())?.let { view.setText(it) }
-                else -> throw IllegalArgumentException("Not implemented yet!")
+            val revealedData = data?.getValue(view.getContentPath())
+            if (revealedData == null) {
+                unrevealedContentPaths.add(view.getContentPath())
+                continue
+            } else {
+                when (view) {
+                    is VGSTextView -> view.setText(revealedData)
+                    else -> throw IllegalArgumentException("Not implemented yet!")
+                }
             }
+        }
+        if (unrevealedContentPaths.isNotEmpty()) {
+            logDebug("Cannot reveal data for contentPaths: $unrevealedContentPaths")
         }
     }
 
     fun isEmpty() = views.isEmpty()
 
     fun getViews() = views
+
+    private fun logStartViewsUpdate() {
+        val contentPaths = views.map {
+            with(it.getContentPath()) {
+                if (isNullOrEmpty()) "CONTENT PATH NOT SET OR EMPTY" else this
+            }
+        }
+        logWaring("Start decoding revealed data for contentPaths: $contentPaths")
+        if (views.any { it.getContentPath().isEmpty() }) {
+            logWaring("Some subscribed views seems to have empty content path. Verify `contentPath` property is set for each view.")
+        }
+    }
 }
