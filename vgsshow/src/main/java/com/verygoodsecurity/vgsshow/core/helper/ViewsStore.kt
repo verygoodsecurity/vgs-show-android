95310ad0b5b64110bc0368a3158f516f514ef548
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-01 13:04:07 +0200

    Refactoring

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/helper/ViewsStore.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/helper/ViewsStore.kt
@@ -2,6 +2,7 @@ package com.verygoodsecurity.vgsshow.core.helper
 
 import androidx.annotation.MainThread
 import com.verygoodsecurity.vgsshow.core.network.model.data.response.ResponseData
+import com.verygoodsecurity.vgsshow.util.extension.logDebug
 import com.verygoodsecurity.vgsshow.util.extension.logWaring
 import com.verygoodsecurity.vgsshow.widget.VGSTextView
 import com.verygoodsecurity.vgsshow.widget.core.VGSView
@@ -56,7 +57,7 @@ internal class ViewsStore {
                 if (isNullOrEmpty()) "CONTENT PATH NOT SET OR EMPTY" else this
             }
         }
-        logWaring("Start decoding revealed data for contentPaths: $contentPaths")
+        logDebug("Start decoding revealed data for contentPaths: $contentPaths")
         if (views.any { it.getContentPath().isEmpty() }) {
             logWaring("Some subscribed views seems to have empty content path. Verify `contentPath` property is set for each view.")
         }
