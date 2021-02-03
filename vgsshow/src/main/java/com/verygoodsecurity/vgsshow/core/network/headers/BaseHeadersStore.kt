40c8efbb4d440c8d0795faa3e6e6972922444f7b
DmytroDm <72735363+DmytroDm@users.noreply.github.com>
2021-02-03 15:12:36 +0200

    Add logLevel param in vgs-client header (#83)
    * Add logLevel in vgs-client header
    
    * Refactoring

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/network/headers/BaseHeadersStore.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/core/network/headers/BaseHeadersStore.kt
@@ -2,12 +2,18 @@ package com.verygoodsecurity.vgsshow.core.network.headers
 
 import com.verygoodsecurity.vgsshow.BuildConfig
 import com.verygoodsecurity.vgsshow.core.Session
+import com.verygoodsecurity.vgsshow.core.logs.VGSShowLogger
 
 internal abstract class BaseHeadersStore : StaticHeadersStore {
 
-    protected open val defaultStaticHeaders: Map<String, String> = mapOf(
-        AGENT_HEADER to "source=show-androidSDK&medium=vgs-show&content=${BuildConfig.VERSION_NAME}&vgsShowSessionId=${Session.id}"
-    )
+    protected open val defaultStaticHeaders: Map<String, String>
+        get() = mapOf(
+            AGENT_HEADER to "source=show-androidSDK" +
+                    "&medium=vgs-show" +
+                    "&content=${BuildConfig.VERSION_NAME}" +
+                    "&vgsShowSessionId=${Session.id}" +
+                    "&logLevel=${if (VGSShowLogger.level == VGSShowLogger.Level.NONE) "none" else "default"}"
+        )
 
     private val staticHeaders: MutableMap<String, String> = HashMap()
 
