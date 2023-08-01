8e9d6e27243666f105584984037c46bdae4133c5
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2023-08-01 17:34:45 +0300

    Update config.yml, add code coverage push script

---
+++ script/publish-code-coverage.sh
@@ -0,0 +1,12 @@
+METRIC_VALUE=$(sed -n '51p' ../vgsshow/build/reports/kover/htmlDebug/index.html)
+
+curl --request POST \
+--url https://verygoodsecurity.atlassian.net/gateway/api/compass/v1/metrics \
+--user "$COMPASS_USER_EMAIL:$COMPASS_API_KEY" \
+--header "Accept: application/json" \
+--header "Content-Type: application/json" \
+--data "{
+  \"metricSourceId\": \"$COMPASS_COMPONENT_URL\",
+  \"value\": $METRIC_VALUE | sed 's/.$//',
+  \"timestamp\": \"$(date -u +'%Y-%m-%dT%H:%M:%SZ')\"
+}"
