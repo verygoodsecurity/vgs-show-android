6be5eeec9b62173bd28423895c5295799bd7b90a
DmytroDm <dmytro.dmytryshyn@vgs.io>
2025-02-12 13:43:05 +0200

    Revert changes (#262)

--- vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/extension/Context.kt
+++ vgsshow/src/main/java/com/verygoodsecurity/vgsshow/widget/extension/Context.kt
@@ -1,11 +1,9 @@
 package com.verygoodsecurity.vgsshow.widget.extension
 
 import android.content.ClipData
-import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.res.TypedArray
-import android.os.PersistableBundle
 import android.util.AttributeSet
 import androidx.core.content.res.use
 
@@ -14,13 +12,8 @@ internal fun Context.getStyledAttributes(
     styleArray: IntArray, block: TypedArray.() -> Unit
 ) = this.obtainStyledAttributes(attributeSet, styleArray).use(block)
 
-private const val CLIP_DATA_CONTENT_SENSITIVE = "android.content.extra.IS_SENSITIVE"
-
 internal fun Context.copyToClipboard(text: String?, label: String = "") {
-    val clip = ClipData.newPlainText(label, text ?: "")
-    (this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(clip)
-    ClipDescription.EXTRA_IS_SENSITIVE
-    clip.description.extras = PersistableBundle().apply {
-        putBoolean(CLIP_DATA_CONTENT_SENSITIVE, true)
-    }
+    (this.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(
+        ClipData.newPlainText(label, text ?: "")
+    )
 }
